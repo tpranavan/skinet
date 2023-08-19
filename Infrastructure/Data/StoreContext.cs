c1e61d87532ad21bdb76fac934cbb45ee3facc14
it20066802 <it20066802@my.sliit.lk>
2023-08-19 19:28:55 +0530

    Repository Pattern completed

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -18,6 +18,10 @@ namespace Infrastructure.Data
         {
         }
 
+        //      public StoreContext(DbContextOptions options) : base(options)
+        // {
+        // }
+
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductBrand> ProductBrands { get; set; }
         public DbSet<ProductType> ProductTypes { get; set; }
